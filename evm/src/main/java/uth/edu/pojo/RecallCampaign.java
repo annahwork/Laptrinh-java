5d04b306e42a2e762d675fea280c16fb7c469a69
Hazuu58 <vominhdat58@gmail.com>
2025-11-04 20:01:15 +0700

    chore: thêm file CampaignService

--- evm/src/main/java/uth/edu/pojo/RecallCampaign.java
+++ evm/src/main/java/uth/edu/pojo/RecallCampaign.java
@@ -1,10 +1,22 @@
 package uth.edu.pojo;
 
-import javax.persistence.*;
-
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 @Table(name = "RecallCampaign")
@@ -58,6 +70,71 @@ public class RecallCampaign {
         this.Status = Status;
         this.Date = Date;
         this.Description = Description;
+    }
+
+    
+    public Integer getCampaignID() {
+        return CampaignID;
+    }
+
+    public void setCampaignID(Integer campaignID) {
+        CampaignID = campaignID;
+    }
+
+    public EVMStaff getCreatedByStaff() {
+        return CreatedByStaff;
+    }
+
+    public void setCreatedByStaff(EVMStaff createdByStaff) {
+        CreatedByStaff = createdByStaff;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String status) {
+        Status = status;
+    }
+
+    public Date getDate() {
+        return Date;
+    }
+
+    public void setDate(Date date) {
+        Date = date;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public List<RecallVehicle> getVehiclesInCampaign() {
+        return VehiclesInCampaign;
+    }
+
+    public void setVehiclesInCampaign(List<RecallVehicle> vehiclesInCampaign) {
+        VehiclesInCampaign = vehiclesInCampaign;
+    }
+
+    public List<Schedule> getSchedules() {
+        return Schedules;
+    }
+
+    public void setSchedules(List<Schedule> schedules) {
+        Schedules = schedules;
     }
 
     public void addVehicle(String VIN)
